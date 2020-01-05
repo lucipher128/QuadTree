16335a1ef206db218cdb45c592d772a15ce205a5
lucipher128 <azizmokadem1414@outlook.fr>
2020-01-05 16:01:58 +0100

    Add files via upload

---
+++ QuadTree.h
@@ -0,0 +1,99 @@
+/*THIS IS A SORTING ALGORITHM THAT PREVENTS FROM EXESSIVE MEMORY
+ACCESS
+
+I BELIEVE 
+*/
+
+#pragma once
+#include<SDL.h>
+#include<iostream>
+
+//=========================POINTS ============================================
+//============================================================================
+
+struct Point {
+private:
+	
+	float x, y;
+public:
+	//CONSTRUCTOR  default is 0 everywhere
+	Point(float X = 0, float Y = 0);
+
+	float GetX();
+	float GetY();
+
+};
+
+//==============================================================================
+//==============================================================================
+//==============================================================================
+//===========================RECTANGLE==========================================
+//==============================================================================
+//==============================================================================
+//==============================================================================
+
+
+class Rectangle {
+private: 
+	
+	
+	float* x, *y;
+	float* w, *h;
+
+
+public:
+
+
+	//CONSTRUCTOR  default is  everywhere
+	Rectangle(float X = 0, float Y = 0, float W = 0, float H = 0);
+	~Rectangle();
+	bool contains(Point p);
+
+	float GetX();
+	float GetY();
+	float GetW();
+	float GetH();
+
+};
+
+
+
+
+
+//==============================================================================
+//==============================================================================
+//==============================================================================
+//===========================QUADTREE==========================================
+//==============================================================================
+//==============================================================================
+//==============================================================================
+
+class QuadTree {
+private:
+	int* nbPoints;
+	Rectangle* rec;
+	bool* divided;
+	QuadTree* nw = NULL;
+	QuadTree* ne = NULL;
+	QuadTree* sw = NULL;
+	QuadTree* se = NULL;
+	const char* name;
+
+	
+	
+	
+	int capacity;
+	Point points[10];
+public:
+	//CONSTRUCTOR  
+	QuadTree(Rectangle rec,int capacity = 2, const char* name = "origin");
+	~QuadTree();
+
+	
+	void SubDivide();
+	void Insert(Point p);
+	void Insert(Point p,std::string __VERION);
+	void search(Point search[10] ,Point p);
+	void draw(SDL_Window* win , SDL_Renderer* ren);
+
+};
---
+++ main.cpp
@@ -0,0 +1,79 @@
+
+
+#include"QuadTree.h"
+#include<iostream>
+#include<time.h>
+#include<random>
+
+
+#define __HEIGHT 700
+#define __WIDTH  800
+
+
+void cap_framerate(Uint32 start) {
+	if ((1000 / 60) > SDL_GetTicks() - start) {
+		SDL_Delay((1000 / 60) - (SDL_GetTicks() - start));
+	}
+}
+
+
+
+
+int main(int argc, char* argv[]) {
+
+	srand(time(NULL));
+
+	SDL_Init(SDL_INIT_EVERYTHING);
+	SDL_Window* window = NULL;
+	SDL_Renderer* Renderer = NULL;
+
+	window = SDL_CreateWindow("test", 200, 200, __WIDTH, __HEIGHT, SDL_WINDOW_RESIZABLE);
+	Renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+
+
+	
+
+
+	
+	
+
+	bool stop = false;
+	Uint32 Start;
+	SDL_Event e;
+	while (!stop) {
+		Start = SDL_GetTicks();
+		while (SDL_PollEvent(&e)) {
+			if (e.type == SDL_QUIT)
+			{
+				stop = true;
+			}
+		}
+
+		Rectangle* rect = NULL;
+		rect = new Rectangle(0, 0, 800, 700);
+
+		QuadTree* test = NULL;
+		test = new QuadTree(*rect, 10);
+
+		for (int i = 0; i < 1000; i++)
+		{
+			Point P(rand() % (int)__WIDTH, rand() % (int)__HEIGHT);
+			//std::cout << P.GetX() << "," << P.GetY() << std::endl;
+			test->Insert(P);
+
+		}
+		test->draw(window, Renderer);
+		SDL_RenderPresent(Renderer);
+		cap_framerate(Start);
+		delete test;
+		delete rect;
+		SDL_SetRenderDrawColor(Renderer, 0, 0, 0, 1);
+		SDL_RenderClear(Renderer);
+	}
+
+	
+
+	SDL_DestroyWindow(window);
+	SDL_DestroyRenderer(Renderer);
+	return 0;
+}
